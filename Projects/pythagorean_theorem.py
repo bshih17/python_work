25cb827ec7167ec83c8e867bb8f7bcfd7c7a537d
Bram Shih <bshihry@gmail.com>
2021-02-04 23:29:48 -0500

    third commit

---
+++ Projects/pythagorean_theorem.py
@@ -0,0 +1,13 @@
+# input
+a = 8
+b = 15
+# c = 5
+
+c = (a**2 + b**2)**.5
+print (f"The Pythagorean Theorem: a^2 + b^2 = c^2")
+
+print (f"If a = {a} and b = {b}, then c = {c}.")
+
+# if we have a and c, but not b, how would I write an if statement?
+if a == 'none' or b == 'none' or c == 'none':
+	print(a)
