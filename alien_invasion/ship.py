910c06ee36989e6d23900cd59fd89b2abe0e3b91
Bram Shih <bshihry@gmail.com>
2021-04-29 21:37:36 -0400

    4/29/21

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,3 +1,6 @@
+# Pygame is efficient because it lets you treat all game elements like rectangles (rects),
+# even if they are not exactly shaped like rectangles.
+
 import pygame
 
 class Ship:
@@ -6,6 +9,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -15,14 +19,29 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Movement flag
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flags
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.x += 1
+        """Update the ship's position based on the movement flags."""
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
