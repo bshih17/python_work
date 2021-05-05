9d50bdd10fc10b6fa539ba29d9f7f518b7db3635
Bram Shih <bshihry@gmail.com>
2021-05-05 15:45:42 -0400

    5/5/21

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -112,6 +112,9 @@ class AlienInvasion:
             # Reset the game statistics
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -180,8 +183,10 @@ class AlienInvasion:
             self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
@@ -189,11 +194,17 @@ class AlienInvasion:
             self._create_fleet()
             self.settings.increase_speed()
 
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
+
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left, and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
+
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -205,6 +216,7 @@ class AlienInvasion:
 
             # Pause.
             sleep(0.5)
+
         else:
             self.stats.game_active = False
             pygame.mouse.set_visible(True)
